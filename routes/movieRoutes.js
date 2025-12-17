d2fddc315407081ecd414530fa70ce821748beac
nourhene <benothmennourhen8@gmail.com>
2025-12-17 18:21:16 +0100

    Update movie routes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,10 +2,5 @@ import express from "express";
 
 const router = express.Router();
 
-// Exemple de route
-router.get("/", (req, res) => {
-  res.json({ message: "Hello from router!" });
-});
-
 // Export du router
 export default router;
