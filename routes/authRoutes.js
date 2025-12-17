cfbb515ec43c677bd047fab1b0d054cf68307f56
nourhene <benothmennourhen8@gmail.com>
2025-12-17 18:21:09 +0100

    Update authentication routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { registerValidation, loginValidation, validateResult } from "../middleware/validate.js";
-import { register,login } from "../controllers/authController.js";
+import { register, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-// Route d'inscription
-router.post("/register", registerValidation, validateResult,register);
+// Route d'inscription avec validation
+router.post("/register", registerValidation, validateResult, register);
 
-// Route de connexion
-router.post("/login", loginValidation, validateResult,login);
+// Route de connexion avec validation
+router.post("/login", loginValidation, validateResult, login);
 
 export default router;
