86713cf58eab4fba7f9a07b11be8bbe67b6aec97
nourhene <benothmennourhen8@gmail.com>
2025-12-17 11:57:09 +0100

    Fix: make Express listen on 0.0.0.0 for Docker and update middleware

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ app.use((err, req, res, next) => {
 // SERVER STARTUP
 // -----------------------------
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   logger.info(`Server running on port ${PORT}`);
 });
