5b4d44f284c40648094dffb0a0655c80702f0d65
nourhene <benothmennourhen8@gmail.com>
2025-12-20 15:55:19 +0100

    refactor: protect watchList routes with authMiddleware

--- routes/watchListRoutes.js
+++ routes/watchListRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 const router=express.Router()
 import {addTowatchList} from "../controllers/watchListController.js"
+import authMiddleware from "../middleware/authMiddleware.js";
+router.use(authMiddleware)
 router.post("/",addTowatchList)
 
 export default router
