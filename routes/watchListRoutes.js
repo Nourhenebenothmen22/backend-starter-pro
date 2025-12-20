a017df3c8d404177b8e0369c5f83f8437daa649b
nourhene <benothmennourhen8@gmail.com>
2025-12-20 15:37:03 +0100

    refactor: update watchListRoutes to integrate watchListController methods

--- routes/watchListRoutes.js
+++ routes/watchListRoutes.js
@@ -1,4 +1,6 @@
 import express from "express";
 const router=express.Router()
+import {addTowatchList} from "../controllers/watchListController.js"
+router.post("/",addTowatchList)
 
 export default router
